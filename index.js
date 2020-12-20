7ebaf4116760c8f45e8923a21db3940692ad2cbb
graciellavl <graciellavl@users.noreply.github.com>
2020-12-20 15:50:52 +0700

    login set

--- index.js
+++ index.js
@@ -117,8 +117,8 @@ function initializeApp() {
 */
 function displayIsInClientInfo() {
     if (liff.isInClient()) {
-        document.getElementById('liffLoginButton').classList.toggle('hidden');
-        document.getElementById('liffLogoutButton').classList.toggle('hidden');
+        document.getElementById('liffLoginButton').classList.add('hidden');
+        document.getElementById('liffLogoutButton').classList.add('hidden');
         // document.getElementById('isInClientMessage').textContent = 'You are opening the app in the in-app browser of LINE.';
     } else {
         // document.getElementById('isInClientMessage').textContent = 'You are opening the app in an external browser.';
