9a1cdf5fa44c1749e63b6be1e202ff0d9e56d2cd
graciellavl <graciellavl@users.noreply.github.com>
2020-12-20 12:52:59 +0700

    change liff id

--- index.js
+++ index.js
@@ -1,6 +1,6 @@
 window.onload = function() {
     const useNodeJS = false;   // if you are not using a node server, set this value to false
-    const defaultLiffId = "1655320175-KDDbwYEk";   // change the default LIFF value if you are not using a node server
+    const defaultLiffId = "1655320175-2W0d17jJ";   // change the default LIFF value if you are not using a node server
 
     // DO NOT CHANGE THIS
     let myLiffId = "";
